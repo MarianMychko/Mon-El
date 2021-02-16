bdc8f8552bc18b14c4b3698d28fd3420efad481d
MarianMychko <marian.mychko@gmail.com>
2021-02-16 22:26:50 +0200

    repaired

--- HW-1/homeworkOne.js
+++ HW-1/homeworkOne.js
@@ -1,69 +1,69 @@
 /* 1 */
 
-const productItemOne = 15.678;
-const productItemTwo = 123.965;
-const productItemThree = 90.2345;
+const tvPrice = 15.678;
+const laptopPrice = 123.965;
+const pcPrice = 90.2345;
 
 /* 2 */
 
-console.log (productItemOne);
-console.log (productItemTwo);
-console.log (productItemThree);
+console.log(tvPrice);
+console.log(laptopPrice);
+console.log(pcPrice);
 
 /* 3 */
 
-const maxPrice = Math.max(productItemOne, productItemTwo , productItemThree);
-const minPrice = Math.min(productItemOne, productItemTwo , productItemThree);
+const maxPrice = Math.max(tvPrice, laptopPrice, pcPrice);
+const minPrice = Math.min(tvPrice, laptopPrice, pcPrice);
 
 /* 4 */
 
-console.log (`Max price ${maxPrice}`);
-console.log (`Min price ${minPrice}`);
+console.log(`Max price ${maxPrice}`);
+console.log(`Min price ${minPrice}`);
 
 /* 5 */
 
-const fullPrice = (productItemOne + productItemTwo + productItemThree);
+const fullPrice = tvPrice + laptopPrice + pcPrice;
 console.log(`Sum of product items ${fullPrice}`);
 
 /* 6 */
 
-const roundMinPrice = Math.floor(productItemOne) + Math.floor(productItemTwo) + Math.floor(productItemThree);
+const roundMinPrice =
+  Math.floor(tvPrice) + Math.floor(laptopPrice) + Math.floor(pcPrice);
 console.log(`Sum rounding price ${roundMinPrice}`);
 
 /* 7 */
 
-const roundTo100Price = Math.round(fullPrice/100)*100;
+const roundTo100Price = Math.round(fullPrice / 100) * 100;
 console.log(`rounding to hundreds ${roundTo100Price}`);
 
 /* 8 */
 
-if (Math.floor(fullPrice) % 2 == 0) {
-console.log('Sum rounding down is a even number.') 
-} else {
-console.log('Sum rounding down is an odd number.')
-};
+const parityNumber = roundMinPrice % 2;
+console.log(`Sum rounding down is ${parityNumber ? "odd" : "even"} number`);
 
 /* 9 */
 
-const sumRest = (500 - productItemOne) + (500 - productItemTwo) + (500 - productItemThree);
+const clientMoney = 500;
+const sumRest =
+  clientMoney - tvPrice + (clientMoney - laptopPrice) + (clientMoney - pcPrice);
 console.log(`The amount of the rest ${sumRest}`);
 
 /* or */
 
-const sumRest1 = (500 - fullPrice)
+const sumRest1 = clientMoney - fullPrice;
 console.log(`The amount of the rest ${sumRest1}`);
 
 /* 10 */
 
 const averagePrice = fullPrice / 3;
-console.log(`Average Price ${averagePrice.toFixed(2)}`);
+console.log(`Average Price ${parseFloat(averagePrice.toFixed(2))}`);
 
 /* 11 */
 
 let discount = Math.random() * fullPrice;
-let personalDiscount = fullPrice - discount ;
-console.log(`Client discount is ${personalDiscount.toFixed(2)}`);
-let netProfit = (fullPrice/2) - personalDiscount.toFixed(2);
+let personalDiscount = fullPrice - discount;
+console.log(`Client discount is ${parseFloat(personalDiscount.toFixed(2))}`);
+let netProfit = fullPrice / 2 - parseFloat(personalDiscount.toFixed(2));
 console.log(`Our net profit is ${netProfit}`);
 
 /* advanced */
@@ -72,15 +72,10 @@ console.log(`Max price ${maxPrice};
 Min price ${minPrice};
 Sum of product items ${fullPrice};
 Sum rounding price ${roundMinPrice};
-Rounding to hundreds ${roundTo100Price};`)
-if (Math.floor(fullPrice) % 2 == 0) {
-    console.log('Sum rounding down is a even number.') 
-    } else {
-    console.log('Sum rounding down is an odd number.')
-    };
-console.log(
-`The amount of the rest ${sumRest}
+Rounding to hundreds ${roundTo100Price};
+Sum rounding down is ${parityNumber ? "odd" : "even"} number
+The amount of the rest ${sumRest}
 The amount of the rest ${sumRest1};
-Average Price ${averagePrice.toFixed(2)};
-Client discount is ${personalDiscount.toFixed(2)};
+Average Price ${parseFloat(averagePrice.toFixed(2))};
+Client discount is ${parseFloat(personalDiscount.toFixed(2))};
 Our net profit is ${netProfit}`);
