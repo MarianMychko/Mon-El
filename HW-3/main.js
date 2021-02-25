8c3763e6485e528cf488758ce2f52abb22580a0a
MarianMychko <marian.mychko@gmail.com>
2021-02-25 22:35:55 +0200

    Update main.js

--- HW-3/main.js
+++ HW-3/main.js
@@ -7,6 +7,18 @@ function getMaxDigit(number) {
 
 //#2
 
+function numberStage(num, stage) {
+  let result = num;
+  if (stage === 1) {
+    result = num;
+  } else {
+    for (let i = 1; i < stage; i++) {
+      result = num * result;
+    }
+  }
+  return result;
+}
+
 //#3
 
 function getCorrectName(name) {
@@ -25,31 +37,27 @@ function getTaxSalary(salary) {
 //#5
 
 function getRandomNumber(numN, numM) {
-return Math.floor(Math.random() * numM ) + numN;
+  return Math.floor(Math.random() * numM) + numN;
 }
 
 //#6
 
 function countLetter(letter, word) {
-    let count = 0;
-  for(let i = 0; i<word.length; i++){
-    if(word[i]===letter) count++;
+  let count = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === letter) count++;
   }
   return count;
 }
 
-//#9
 
-/* deleteLetters(letter, word) {
+//#7
 
-}
 
-*/
-
-//results
 
 document.writeln(`Функція №1: ${getMaxDigit(25788485635876853)} <br>`);
+document.writeln(`Функція №2: ${numberStage(4, 3)}<br>`);
 document.writeln(`Функція №3: ${getCorrectName('mArIaN')} <br>`);
 document.writeln(`Функція №4: ${getTaxSalary(1000)} <br>`);
 document.writeln(`Функція №5: ${getRandomNumber(15, 53)} <br>`);
-document.writeln(`Функція №6: ${countLetter("r", "cursor family")} <br>`);
+document.writeln(`Функція №6: ${countLetter('r', 'cursor family')} <br>`);
