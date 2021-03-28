daea350ba67cfe742da8bb7c490bc5b2e42adece
MarianMychko <marian.mychko@gmail.com>
2021-03-28 07:06:18 +0300

    Update

---
+++ Hw-12/main.js
@@ -0,0 +1,15 @@
+async function getRandomChinese(length) {
+    let counter = 0;
+    let result = '';
+    while (counter < length) {
+        await new Promise((resolve) => {
+            result += String.fromCharCode(Date.now()).slice(-5);
+            counter++;
+            setTimeout(() => {
+                resolve(result)
+            }, 50);
+        });
+    };
+    console.log(result);
+};
+getRandomChinese(4);
