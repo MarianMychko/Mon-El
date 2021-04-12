eaeef057975d27bd6b32804bda537d35f0c0c54a
MarianMychko <marian.mychko@gmail.com>
2021-04-12 22:05:38 +0300

    Update

--- HW-10/main.js
+++ HW-10/main.js
@@ -1,19 +1,62 @@
-function generateBlocks() {
-    const grid = document.querySelector('.grid');
-    grid.style.display = 'grid';
-    blockRandomColor();
+const limit = 5;
+
+function blocks() {
+    const template = document.createElement('div');
+    template.classList.add('template')
+    for (let i = 0; i < limit; i++) {
+
+        document.body.append(template)
+        const row = document.createElement('div');
+        row.classList.add('row');
+        row.style.display = 'flex'
+        template.appendChild(row);
+
+        for (let j = 0; j < limit; j++) {
+            const squere = document.createElement("div");
+            squere.classList.add("squere");
+
+            row.appendChild(squere);
+
+            function getRandomColor() {
+                return `#${(Math.random().toString(16) + '000000').substring(2,8).toUpperCase()}`;
+            }
+            squere.style.backgroundColor = getRandomColor();
+            squere.style.height = "50px"
+            squere.style.width = "50px"
+            squere.style.margin = "1px"
+            squere.style.border = "2px solid #000"
+        }
+
+    }
 }
 
-// Advanced
-function blockRandomColor() {
-    const grid = document.querySelectorAll('.block');
-    grid.forEach(item => item.style.backgroundColor = `rgb(${getRandomColor(0, 255)}, 
-                                                                    ${getRandomColor(0, 255)}, 
-                                                                    ${getRandomColor(0, 255)})`)
+
+function newSqueres() {
+    if (!document.querySelector('.template')) {
+        blocks();
+    }
+    document.querySelector('.template').remove();
+    blocks()
 }
-function getRandomColor(min, max){
-    return Math.ceil(Math.random() * (max - min) + min);
+
+let abort = false
+
+function dynamicSqures() {
+
+    abort = false
+    setInterval(() => {
+        if (abort == true) {
+            return
+        }
+        if (!document.querySelector('.template')) {
+            blocks();
+        }
+        document.querySelector('.template').remove();
+        blocks()
+
+    }, 1000)
 }
-function generateBlocksInterval(time = 1000) {
-    setInterval(() => blockRandomColor(), time);
+
+function stopDynamicSqures() {
+    abort = true
 }
