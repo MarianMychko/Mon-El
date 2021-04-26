ffcd4fd5f9b05ac0cc4007ac4536f7117bc3cd81
MarianMychko <marian.mychko@gmail.com>
2021-04-26 23:17:46 +0300

    recover homework 11

--- HW-11/main.js
+++ HW-11/main.js
@@ -1,42 +1,21 @@
-const container = document.createElement('div');
-container.className = 'container';
-document.body.append(container);
-
-const chordNames = ['Q', 'W', 'E', 'R', 'T', 'Y'];
-
-function generateButtons() {
-  let button;
-
-  for (let i = 0; i < 6; i++) {
-    button = document.createElement('button');
-    button.className = chordNames[i];
-    button.innerHTML = chordNames[i];
-    container.appendChild(button);
-  }
-  return container;
-}
-generateButtons();
-
-container.addEventListener('mousedown', function (event) {
-  event.target.style = 'border-color: rgb(150, 35, 45);';
-  new Audio(`./Sounds/${event.target.className}.mp3`).play();
-});
-
-container.addEventListener('mouseup', function (event) {
-  event.target.style = 'border-color: rgb(0, 0, 0);';
-});
-
-document.addEventListener('keydown', function (event) {
-  if (chordNames.includes(event.key.toUpperCase())) {
-    var element = document.getElementsByClassName(event.key.toUpperCase())[0];
-    element.style = 'border-color: rgb(150, 35, 45);';
-    new Audio(`./Sounds/${event.key.toUpperCase()}.mp3`).play();
-  }
-});
-
-document.addEventListener('keyup', function (event) {
-  if (chordNames.includes(event.key.toUpperCase())) {
-    var element = document.getElementsByClassName(event.key.toUpperCase())[0];
-    element.style = 'border-color: rgb(0, 0, 0);';
-  }
-});
+const classKey = document.querySelector('.key');
+const classSounds = document.querySelector('.sounds');
+document.addEventListener('keydown', function clickKey(event) {
+    const key = '.' + event.code
+    if (classKey.querySelector(key) === null)
+    return;
+    classKey.querySelector(key).classList.add('style');
+    classSounds.querySelector(key).play();
+})
+document.addEventListener('keyup', function clickKey(event) {
+    const key = '.' + event.code;
+    if (classKey.querySelector(key) === null)
+    return;
+    classKey.querySelector(key).classList.remove('style');
+})
+document.addEventListener('click', function clickMouse(event) {
+    const key = '.' + event.target.className;
+    if (classKey.querySelector(key) === null)
+    return;
+    classSounds.querySelector(key).play();
+})
